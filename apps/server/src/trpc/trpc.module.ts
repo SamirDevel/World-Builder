57139bb8c6c5156e3eaeb00b93ac95eaf5ee1d30
Samir Priego <samir.adevs@gmail.com>
2024-10-03 23:17:11 -0600

    0.1
    Base para el proyecto

--- apps/server/src/trpc/trpc.module.ts
+++ apps/server/src/trpc/trpc.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TrpcService } from '@server/trpc/trpc.service';
+import { TrpcRouter } from './trpc.router';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [],
+  providers: [TrpcService, TrpcRouter],
 })
 export class TrpcModule {}
